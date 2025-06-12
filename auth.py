d3ce109e6f0fdbe94a64ea8de91e3ecec159f819
Pandan-16 <momotaro.teay@gmail.com>
2025-06-12 22:21:01 +0700

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,30 @@
+import json
+import os
+import hashlib
+
+USER_FILE = "users.json"
+
+def load_users():
+    if os.path.exists(USER_FILE):
+        with open(USER_FILE, "r") as f:
+            return json.load(f)
+    return {}
+
+def save_users(users):
+    with open(USER_FILE, "w") as f:
+        json.dump(users, f)
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def login_user(username, password):
+    users = load_users()
+    return username in users and users[username] == hash_password(password)
+
+def register_user(username, password):
+    users = load_users()
+    if username in users:
+        return False
+    users[username] = hash_password(password)
+    save_users(users)
+    return True

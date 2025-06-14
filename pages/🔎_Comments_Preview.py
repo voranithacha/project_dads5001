f399b1c34533c4e1f7ac64d53fe5b627aa52b78d
Seriphap Siangnok (Way) <siangnok@gmail.com>
2025-06-14 23:59:20 +0700

    Update 🔎_Comments_Preview.py

--- pages/🔎_Comments_Preview.py
+++ pages/🔎_Comments_Preview.py
@@ -1,14 +1,11 @@
 import streamlit as st
 
-
 # สร้างเมนูด้านข้าง
 sub_page = st.sidebar.radio("เลือกหน้า Word Cloud", ["Word Cloud", "subpage2", "subpage3", "subpage4", "subpage5"])
 
 # เงื่อนไขการแสดงผลตามหน้าที่เลือก
 if sub_page == "Word Cloud":
     from pages.Comment_Preview import Word_Cloud
-    Word_Cloud.show()
-
 elif sub_page == "subpage2":
     st.write("คุณเลือกหน้า subpage2")
 elif sub_page == "subpage3":

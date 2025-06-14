4d8b59ee497fe22a2541e80c8634ce370f77c9c2
Seriphap Siangnok (Way) <siangnok@gmail.com>
2025-06-15 01:57:13 +0700

    Update Count_Comments_Monthly.py

--- pages/Comment_Preview/Count_Comments_Monthly.py
+++ pages/Comment_Preview/Count_Comments_Monthly.py
@@ -5,17 +5,17 @@ import duckdb
 import plotly.express as px
 
 def run():
-  # === PATH CONFIG ===
-  csv_path = './data/youtube_comments_full.csv'
-  # ตรวจสอบว่าไฟล์มีอยู่หรือไม่
-  if os.path.exists(csv_path):
-      # โหลดข้อมูลจาก CSV
-      df = pd.read_csv(csv_path)
-   
-      # แสดงตัวอย่างข้อมูล 5 แถวแรก
-      st.dataframe(df.head())
-  else:
-      st.error(f"ไม่พบไฟล์ที่ตำแหน่ง: {csv_path}")
+          # === PATH CONFIG ===
+          csv_path = './data/youtube_comments_full.csv'
+          # ตรวจสอบว่าไฟล์มีอยู่หรือไม่
+          if os.path.exists(csv_path):
+              # โหลดข้อมูลจาก CSV
+              df = pd.read_csv(csv_path)
+           
+              # แสดงตัวอย่างข้อมูล 5 แถวแรก
+              st.dataframe(df.head())
+          else:
+              st.error(f"ไม่พบไฟล์ที่ตำแหน่ง: {csv_path}")
   
  
 

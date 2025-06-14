8e29f883da89ad7fc807da5e362003ee5b82b3d3
Seriphap Siangnok (Way) <siangnok@gmail.com>
2025-06-15 02:06:30 +0700

    Update Count_Comments_Monthly.py

--- pages/Comment_Preview/Count_Comments_Monthly.py
+++ pages/Comment_Preview/Count_Comments_Monthly.py
@@ -1,19 +1,12 @@
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
-from pythainlp.tokenize import word_tokenize
-from pythainlp.tag import pos_tag
-from sklearn.feature_extraction.text import CountVectorizer
 import re
 import os
 
 def run():
     # === PATH CONFIG ===
     CSV_PATH = './data/youtube_comments_full.csv'
-    FONT_PATH = './fonts/Sarabun-Regular.ttf'
-    
-    st.subheader("☁️ Word Cloud จากความคิดเห็นใน YouTube")
+    st.subheader("⌨ จำนวนการแสดงความคิดเห็นในเเต่ละเดือน")
     
     # === FILE CHECK ===
     if not os.path.exists(CSV_PATH):

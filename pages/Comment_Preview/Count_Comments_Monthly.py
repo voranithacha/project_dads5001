f16a0b6f70ef7161ca73b413d6387f96f50de77f
Seriphap Siangnok (Way) <siangnok@gmail.com>
2025-06-15 01:44:34 +0700

    Update Count_Comments_Monthly.py

--- pages/Comment_Preview/Count_Comments_Monthly.py
+++ pages/Comment_Preview/Count_Comments_Monthly.py
@@ -2,9 +2,10 @@ import pandas as pd
 import streamlit as st
 import os
 
-# === PATH CONFIG ===
-CSV_PATH = './data/youtube_comments_full.csv'
-
-st.subheader("📊 จำนวนการแสดงความคิดเห็นในแต่ละเดือน (CSV)")
+def run():
+  # === PATH CONFIG ===
+  CSV_PATH = './data/youtube_comments_full.csv'
+  
+  st.subheader("📊 จำนวนการแสดงความคิดเห็นในแต่ละเดือน (CSV)")
 
 

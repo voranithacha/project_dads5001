8b6728a957743aa311e394288a02f247aca7181a
Seriphap Siangnok (Way) <siangnok@gmail.com>
2025-06-15 00:23:06 +0700

    Update 🔎_Comments_Preview.py

--- pages/🔎_Comments_Preview.py
+++ pages/🔎_Comments_Preview.py
@@ -1,10 +1,18 @@
 import streamlit as st
 
 # สร้างเมนูด้านข้าง
-sub_page = st.sidebar.radio("เลือกหัวข้อการ Preview Comments", ["Word Cloud", "subpage2", "subpage3", "subpage4", "subpage5"])
+sub_page = st.sidebar.radio("เลือกหัวข้อการ Preview Comments", ["จำนวนการ comments เเต่ละเดือน",
+                                                             "Word Cloud",
+                                                             "Top5 Comments ที่มีจำนวน Like เยอะที่สุด", 
+                                                             "Top5 Comments ที่มีจำนวน Reply เยอะที่สุด", 
+                                                             "Top 5 users ที่มีจำนวน comments เยอะที่สุด",
+                                                             "Top5 Comments ที่มีความยาวมากที่สุด",
+                                                             ])
 
 # เงื่อนไขการแสดงผลตามหน้าที่เลือก
-if sub_page == "Word Cloud":
+if sub_page == "จำนวนการ comments เเต่ละเดือน":
+    
+elif sub_page == "Word Cloud":
     from pages.Comment_Preview import Word_Cloud
 elif sub_page == "subpage2":
     st.write("คุณเลือกหน้า subpage2")

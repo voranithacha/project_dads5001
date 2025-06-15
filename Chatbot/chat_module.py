fe09b997114c4b675b1daf7a0cbbe64628764009
Pandan-16 <momotaro.teay@gmail.com>
2025-06-15 11:10:33 +0700

    Delete Chatbot directory

--- Chatbot/chat_module.py
+++
@@ -1,8 +0,0 @@
-def ask_bot(message):
-   
-    if "สวัสดี" in message:
-        return "สวัสดีครับ ยินดีช่วยเหลือ!"
-    elif "ชื่ออะไร" in message:
-        return "ฉันคือ AskAI จาก DADS5001"
-    else:
-        return "ขออภัย ฉันยังไม่เข้าใจคำถามนี้"
